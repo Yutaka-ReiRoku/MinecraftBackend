28e4fe2b0d1b0dbab55defe184b325783bc42b31
Yutaka ReiRoku <ledaigiadevilgamer@gmail.com>
2025-12-20 03:37:49 +0700

    update

---
+++ WEB/MinecraftBackend/MinecraftBackend/Migrations/20251219203719_AddMonstersRecipes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinecraftBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMonstersRecipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Monsters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    HP = table.Column<int>(type: "INTEGER", nullable: false),
+                    Damage = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpReward = table.Column<int>(type: "INTEGER", nullable: false),
+                    GoldReward = table.Column<int>(type: "INTEGER", nullable: false),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Monsters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RecipeId = table.Column<string>(type: "TEXT", nullable: false),
+                    ResultItemId = table.Column<string>(type: "TEXT", nullable: false),
+                    ResultItemName = table.Column<string>(type: "TEXT", nullable: false),
+                    ResultItemImage = table.Column<string>(type: "TEXT", nullable: false),
+                    CraftingTime = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ingredients = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Monsters");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
